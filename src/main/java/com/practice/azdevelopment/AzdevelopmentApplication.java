8d6ff1b784a514f879aaafded230644477bad561
Shylesh.pasupathi <shyleshshyl02@gmail.com>
2025-06-28 21:19:06 +0530

    first commit

---
+++ src/main/java/com/practice/azdevelopment/AzdevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.practice.azdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzdevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzdevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/azdevelopment/AzdevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.azdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzdevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
